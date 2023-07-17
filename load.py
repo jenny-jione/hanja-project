3ee1560637d97270a83b6e300b81ad5ab7e91b41
Jione Jang <jenny.jione.jang@gmail.com>
2023-07-17 17:00:39 +0900

    feat: separate load_file()

---
+++ load.py
@@ -0,0 +1,14 @@
+import csv
+
+PATH_TEST = './data/test'
+
+
+def load_file():
+    with open('./data/data_part.csv', 'r') as f:
+        rdr = csv.reader(f)
+        next(rdr)
+        
+        tuple_list = []
+        for row in rdr:
+            tuple_list.append(tuple(row))
+    return tuple_list
