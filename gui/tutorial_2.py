bf44591357c3f2ba487e17ca58b61757fe683491
Jione Jang <jenny.jione.jang@gmail.com>
2023-07-18 17:32:05 +0900

    add: gui sample code

---
+++ gui/tutorial_2.py
@@ -0,0 +1,42 @@
+# tkinter 패키지 import
+import tkinter as tk
+from time import sleep
+
+# Tk Class 선언으로 window 창 생성
+window = tk.Tk()
+
+# 생성할 window 창의 크기 및 초기 위치 설정 매서드: geometry()
+window_width = 400
+window_height = 200
+window_pos_x = 700
+window_pos_y = 100
+
+window.geometry("{}x{}+{}+{}".format(window_width, window_height, window_pos_x, window_pos_y))
+
+# 생성한 Window 창의 크기 조절 가능 여부 설정: resizable()
+window.resizable(False, False)   # True, False 대신 1, 0을 사용할 수 있음
+
+# 생성한 Window 창의 Title 설정: title()
+window.title("Tkinter: Test")
+
+# 생성한 Window 창의 Icon 설정: iconphoto()
+# window.iconphoto(False, tk.PhotoImage(file="icon1.png"))
+
+# tk.Label 클래스 선언 및 Button 위젯 생성
+label1 = tk.Label(window, text="한자", anchor="w")
+label2 = tk.Label(window, text="여긴 답 나옴", bg="skyblue", relief="solid", bd=1)
+
+b1 = tk.Button(window, text='정답')
+# 생성한 Label 위젯을 pack() 매서드로 배치
+label1.pack()
+label2.pack()
+b1.pack()
+
+def btn_click(event):
+    print('click!')
+
+b1.bind('<Button-1>', btn_click)
+
+
+# 생성한 창을 유지하기 위한 코드 작성
+window.mainloop()
--- gui_study.py
+++ gui_study.py
@@ -3,7 +3,7 @@ from time import sleep
 
 window = tk.Tk()
 window_width = 100
-window_height = 100
+window_height = 120
 window_pos_x = 1800
 window_pos_y = 1000
 window.geometry("{}x{}+{}+{}".format(window_width, window_height, window_pos_x, window_pos_y))
@@ -15,34 +15,36 @@ from modules.load import load_file
 li = load_file()
 
 from modules.shuffle import HANJA_IDX, HMS_IDX, LEVEL_IDX
-import random
-cur_idx = 0
-random.shuffle(li)
-li = li[:5]
-
+click = 0
 def update_labels():
-    global li, cur_idx
-    if cur_idx < len(li):
-        hanja = li[cur_idx][HANJA_IDX]
-        answer = li[cur_idx][HMS_IDX]
-        # label2_text.set(hanja)
+    global li, click
+    if click < len(li):
+        hanja = li[click][HANJA_IDX]
+        answer = li[click][HMS_IDX]
+        level = li[click][LEVEL_IDX]
         label1.config(text=hanja)
         label2.config(text=answer)
-        cur_idx += 1
+        label_cnt.config(text=str(click))
+        label_lv.config(text=level)
+        click += 1
     else:
         label1.config(text='')
         label2.config(text='-The End-')
+        label_cnt.config(text='')
+        label_lv.config(text='')
+
 
 label1 = tk.Label(window, text=" ", anchor="w")
 label2 = tk.Label(window, text=" ")
-# label2_text = tk.StringVar()
-# label2_text.set('a')
-# label2 = tk.Label(window, textvariable=label2_text)
+label_lv = tk.Label(window, text=" ")
+label_cnt = tk.Label(window, text=" ")
 
 b1 = tk.Button(window, text='btn', command=update_labels)
 
 label1.pack()
 label2.pack()
+label_lv.pack()
+label_cnt.pack()
 b1.pack()
 
 window.mainloop()
