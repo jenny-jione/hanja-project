1624afa486e56152ca65d316525ce9b69c64b7cb
jenny.jione.jang <jenny.jione.jang@gmail.com>
2023-07-21 21:56:04 +0900

    fix: self.cur_idx가 1부터 시작하는 오류 고침 (원인: start 버튼에 바인딩된 updated_labels)

--- gui_input.py
+++ gui_input.py
@@ -16,8 +16,7 @@ class ReadingTest:
         self.label_new = tk.Label(window, text=" ", font=normal_font)
         window.grid_columnconfigure(0, weight=1)  # Column 0 will expand to center-align elements
         window.grid_columnconfigure(1, weight=1)  # Column 1 will also expand for label_new
-        self.start = tk.Button(window, text='start',
-                                command=lambda s=self: s.update_labels())
+        random.shuffle(li)
         self.entry = tk.Entry(window)
         self.entry.bind('<Return>', self.show_text)
         self.label_han.grid(row=0, column=0, columnspan=2)  # Set columnspan to 2 to span both columns
@@ -25,19 +24,18 @@ class ReadingTest:
         self.label_lev.grid(row=2, column=0, columnspan=2)
         self.label_cnt.grid(row=3, column=0, sticky="e")  # Use sticky="e" for right-align
         self.label_new.grid(row=3, column=1, sticky="w")  # Use sticky="w" for left-align
-        self.start.grid(row=5)
-        random.shuffle(li)
+        self.label_han.config(text=li[0][HANJA_IDX])
+        self.entry.grid(row=4, columnspan=2)
         self.result = []
     
     def show_text(self, event):
         response = self.entry.get()
         han, kor, lev = self.get_data()
-        f_str = f'[{str(self.cur_idx)}/{len(li)}] '
+        f_str = f'[{str(self.cur_idx+1)}/{len(li)}] '
         if kor == response:
             f_str = f_str + 'right!'
         else:
             f_str = f_str + f'wrong:: {han} {kor}'
-            # self.result.append((han, kor, lev))
             dic = {
                 'han': han,
                 'kor': kor,
@@ -48,18 +46,19 @@ class ReadingTest:
         self.entry.delete(0, tk.END)
         self.update_labels()
 
+    # TODO: 0부터 시작하는게 아니라 idx가 1부터 시작돼서..
+    #  맨 첫번째 있는 한자는 아예 건너뛰는 현상 고치기 => 완료!
     def update_labels(self):
         self.cur_idx += 1
         if self.cur_idx > 0:
-            self.start.grid_forget()
-            self.entry.grid(row=4)
+            self.entry.grid(row=4, columnspan=2)
         if self.cur_idx < len(li):
             han, _, _ = self.get_data()
             self.label_han.config(text=han)
         else:
             for res in self.result:
                 print(res)
-            print(f'result:{len(li)-len(self.result)} /{len(li)}')
+            print(f'result:{len(li)-len(self.result)}/{len(li)}')
             self.save_result()
             self.remove_labels()
     
@@ -84,7 +83,6 @@ class ReadingTest:
         han = li[self.cur_idx][HANJA_IDX]
         level_raw = li[self.cur_idx][LEVEL_IDX]
         level = refactor_data(level_raw)
-        # self.label_new.config(text='/ ' + str(len(li)))
         return han, kor, level
 
 
