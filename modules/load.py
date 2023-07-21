455ff4c6da7d2fbe8d18f2c569dae9fc8aed530a
Jione Jang <jenny.jione.jang@gmail.com>
2023-07-21 15:09:31 +0900

    create: load_file__new i n g . .

--- modules/load.py
+++ modules/load.py
@@ -11,13 +11,38 @@ def load_file():
             tuple_list.append(tuple(row))
     return tuple_list
 
+def load_file__new():
+    with open('./data/data_today.csv', 'r') as f:
+        rdr = csv.reader(f)
+        
+        lines = list(rdr)
+        header = lines[0]
+        IDX_LEV = header.index('level')
+        IDX_HAN = header.index('hanja')
+        IDX_KOR = header.index('hms')
+        
+        result = []
+        for line in lines[1:]:
+            data = {
+                'level': line[IDX_LEV],
+                'hanja': line[IDX_HAN],
+                'kor': line[IDX_KOR]
+            }
+            result.append(data)
+    return result
+
+
 """
 TODO
 1. gui에서 범위 선택할 수 있게 하기
-  1-1) 혹은 처음에는 더 쉽게, 1번:ㄱ, 2번:ㄴ, 선택
-  1-2) 선택한 범위에 맞는 .csv 파일을 가져오기.
+    1-1) 혹은 처음에는 더 쉽게, 1번:ㄱ, 2번:ㄴ, 선택
+    1-2) 선택한 범위에 맞는 .csv 파일을 가져오기.
 2. 한자 검색 기능
 3. 현재: 한자, 훈, 음, 급수
-   목표: 비슷한 한자, 부수, 단어 예시 등.. 더 보여주기
-        근데 이건 크롤링 더 해서 데이터를 쌓아야 할듯!!
+    목표: 비슷한 한자, 부수, 단어 예시 등.. 더 보여주기
+    근데 이건 크롤링 더 해서 데이터를 쌓아야 할듯!!
 """
+
+if __name__ == '__main__':
+    li = load_file__new()
+    print(li)
