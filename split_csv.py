5e634120634e731d6f47dc0b34c0a193efd932bb
Jione Jang <jenny.jione.jang@gmail.com>
2023-07-24 16:54:58 +0900

    feat: split_list function to split and store list elements in chunks

---
+++ split_csv.py
@@ -0,0 +1,30 @@
+import csv
+import random
+from modules.load import load_file
+from modules.shuffle import HEADER_ROW
+
+FILE_PATH = './data/data_review.csv'
+
+def split_list(li: list, chunk: int):
+    print(len(li))
+    num = len(li) // chunk
+    for i in range(num+1):
+        start_idx = i*chunk
+        end_idx = (i+1)*chunk
+        print(f'{start_idx}:{end_idx}')
+        sub_li = li[start_idx:end_idx]
+        create_sub_file(sub_li, i)
+
+
+def create_sub_file(li: list, idx: int):
+    with open(f'./data/data_review_split_{str(idx+1).zfill(2)}.csv', 'w') as f:
+        wr = csv.writer(f)
+        wr.writerow(HEADER_ROW)
+        for row in li:
+            wr.writerow(row)
+
+
+if __name__ == '__main__':
+    li = load_file(FILE_PATH)
+    random.shuffle(li)
+    split_list(li, 100)
