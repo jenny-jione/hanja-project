102052b5eaae311e0bc32d9015c27038833c980d
Jione Jang <jenny.jione.jang@gmail.com>
2023-09-26 17:20:50 +0900

    chore: load file 변경

--- gui_test.py
+++ gui_test.py
@@ -8,16 +8,19 @@ from modules.index import (
 import random
 import time
 import csv
-from modules.load import load_today_file, load_file
+from modules.load import load_today_file, load_file, load_all_file_with_radical
 # li = load_today_file()
-li = load_file('./data/seng.csv')
-LEN = len(li)
-idx = 9
-CHUNK = 50
-start_idx = idx*CHUNK
-end_idx = (idx+1)*CHUNK
-print(f'{LEN} -- {start_idx}:{end_idx}')
-li = li[start_idx:end_idx]
+# li = load_file('./data/seng.csv')
+li = load_all_file_with_radical()
+# LEN = len(li)
+# idx = 9
+# CHUNK = 50
+# start_idx = idx*CHUNK
+# end_idx = (idx+1)*CHUNK
+# print(f'{LEN} -- {start_idx}:{end_idx}')
+# li = li[start_idx:end_idx]
+random.shuffle(li)
+li = li[:50]
 
 ROW_HANJA = 0
 ROW_KOR = 1
