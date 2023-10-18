b189f9bd6c32a36ee0306ae1fdece20b0a742176
Jione Jang <jenny.jione.jang@gmail.com>
2023-10-18 17:08:34 +0900

    feat: mistake 통계

---
+++ stats.py
@@ -0,0 +1,14 @@
+import csv
+from modules.load import load_file
+from modules.index import HEADER_ROW
+
+data = load_file('./data/accumulated_results.csv')
+print(type(data))
+print(len(data))
+
+data_sorted = sorted(data, key=lambda x: x[-1], reverse=True)
+
+for row in data_sorted[:20]:
+    print(row[0], row[-1])
+
+# hanja,kor,radical,radical_name,stroke_count,level,rep_pron,mistake
