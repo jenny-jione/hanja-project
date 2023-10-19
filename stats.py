a36176db05e853dbf408063db4eb4788aecc6629
Jione Jang <jenny.jione.jang@gmail.com>
2023-10-19 14:58:27 +0900

    fix: 정렬기준 int()추가 csv를 읽은 결과가 문자열이기 때문에 int로 변환하는 과정 추가

--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ data = load_file('./data/accumulated_results.csv')
 print(type(data))
 print(len(data))
 
-data_sorted = sorted(data, key=lambda x: x[-1], reverse=True)
+data_sorted = sorted(data, key=lambda x: int(x[-1]), reverse=True)
 
 for row in data_sorted[:20]:
     print(row[0], row[-1])
