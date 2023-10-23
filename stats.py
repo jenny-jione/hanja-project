757a13d55c19526bac2786c73a87ab2711edf0ff
Jione Jang <jenny.jione.jang@gmail.com>
2023-10-23 16:16:45 +0900

    feat: 모듈화

--- stats.py
+++ stats.py
@@ -2,13 +2,29 @@ import csv
 from modules.load import load_file
 from modules.index import HEADER_ROW
 
-data = load_file('./data/accumulated_results.csv')
-print(type(data))
-print(len(data))
 
-data_sorted = sorted(data, key=lambda x: int(x[-1]), reverse=True)
+def get_grade_stats():
+    data = load_file('./data/accumulated_results.csv')
+    print(type(data))
+    print(len(data))
 
-for row in data_sorted[:20]:
-    print(row[0], row[-1])
+    data_sorted = sorted(data, key=lambda x: int(x[-1]), reverse=True)
 
-# hanja,kor,radical,radical_name,stroke_count,level,rep_pron,mistake
+    for row in data_sorted[:20]:
+        print(row[0], row[-1])
+
+
+def get_time_stats():
+    with open('./data/elasped.csv', 'r') as f:
+        rdr = csv.reader(f)
+        header = next(rdr)
+        print(header)
+        data = []
+        for row in rdr:
+            avg = int(row[header.index('elasped')]) / int(row[header.index('total')])
+            data.append(avg)
+        print(data)
+
+if __name__ == "__main__":
+    # get_grade_stats()
+    get_time_stats()
