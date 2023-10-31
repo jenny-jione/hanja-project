16a77bba50e80a5a840faf6f1acf4e56b42dba9a
Jione Jang <jenny.jione.jang@gmail.com>
2023-10-31 16:53:13 +0900

    ing: memo_ab.csv unique 작업중

--- process_memo.py
+++ process_memo.py
@@ -62,8 +62,67 @@ def save_file(data: list):
         wr = csv.writer(f)
         for row in data:
             wr.writerow(row)
+
+
+def get_ab_han():
+    with open('./memo_ab_sort.csv', 'r') as f:
+        rdr = csv.reader(f)
+        result = list(rdr)
+    return result
+
+
+def save_sort_file(data: list):
+    with open('./memo_ab_sort.csv', 'w') as f:
+        wr = csv.writer(f)
+        for row in data:
+            wr.writerow(row)
+
+
+# def check_subset(onerow: list, data: list, idx: int):
+#     unique = True
+#     for j, row in enumerate(data):
+#         if set(onerow).issubset(row):
+#             unique = False
+#     return unique
         
 
+# TODO: ing !!! unique 1, 0, 2 -> true, false, same
+def check_subset(onerow: list, data: list, idx: int):
+    unique = True
+    for j, row in enumerate(data):
+        # if onerow == row:
+        #     unique = 2
+        if set(onerow).issubset(row):
+            unique = False
+    return unique
+
+
+def get_unique_list(data: list):
+    data_1 = data[:]
+    res = []
+    result = []
+    print(f'before: {len(data)}')
+    for i, row in enumerate(data_1):
+        unique = check_subset(row, data[:i]+data[i+1:], i)
+        if unique:
+            res.append(row)
+    
+    print(f'after : {len(res)}')
+    
+    # unique
+    for r in res:
+        if r not in result:
+            result.append(r)
+    
+    print(f'final: {len(result)}')
+    
+    return result
+
+
 if __name__ == "__main__":
-    data_ab = get_ab()
-    save_file(data_ab)
+    # data_ab = get_ab()
+    # save_file(data_ab)
+    
+    unique_list: list = get_unique_list(get_ab_han())
+    # save_sort_file(unique_list)
+    
